6823600a68c5ec300896e1a3f09dbf4cc13e0cb9
Martin Acevedo <martin.acevedo@fibertel.com.ar>
2020-09-27 19:51:53 -0300

    First commit

---
+++ html/images/index.pl
@@ -0,0 +1,14 @@
+#!/usr/bin/perl
+
+print "Content-type:text/html\n\n";
+print "<h1>Puta madre anduvo</h1>";
+
+for (my $i=0;$i<10;$i++){
+    print "<p>$i</p>";
+}
+
+print "<h3>vamooo</h3>";
+
+exit 0;
+
+1;
