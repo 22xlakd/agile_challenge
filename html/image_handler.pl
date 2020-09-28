2fda5800b767361cd749f4781336478b839d6e6d
Martin Acevedo <martin.acevedo@fibertel.com.ar>
2020-09-28 02:46:10 -0300

    Finished after 2 hours

--- html/image_handler.pl
+++ html/image_handler.pl
@@ -7,11 +7,22 @@ use warnings;
 use strict;
 
 my $request = CGI->new;
-my $image_id   = $request->param('id') || undef;
+my $image_id = $request->param('id') || undef;
+my $page = $request->param('page') || undef;
 
 my $image_service = ImageService->new();
 
-return $image_service->get_image(id => $image_id);
+$image_service->get_all_images();
+
+print $request->header('application/json');
+
+if($image_id){
+    return $image_service->get_single_image(id => $image_id);
+}
+else{
+    print $image_service->get_images(page => $page);
+}
+
 
 exit 0;
 
--- libs/ImageService.pm
+++ libs/ImageService.pm
@@ -1,17 +1,84 @@
 package ImageService;
 
+use LWP::UserAgent;
+use JSON::XS;
+use HTTP::Request;
+
+use Data::Dumper;
+
+use warnings;
+use strict;
+
+our $ACCESS_TOKEN = undef;
+our $IMAGES = undef;
+
+use constant ITEMS_PER_PAGE => 5;
+use constant PAGE => 1;
+
 sub new {
     my $class = shift;
     return bless {}, $class;
 }
 
-sub get_image {
+sub get_all_images {
+    my ($self, %args) = @_;
+
+    my $body = {
+        'apiKey'  => '23567b218376f79d9415'
+    };
+
+    my $access_token_response = $self->_execute(
+        endpoint    => 'http://interview.agileengine.com',
+        method      => 'POST',
+        path        => '/auth',
+        body        => $body
+    );
+
+    if($access_token_response->{auth} eq JSON::XS::true){
+        $ACCESS_TOKEN = $access_token_response->{token};
+
+        # HERE I ADDED THE LIMIT PARAM BECAUSE I COULDN'T FIND A METHOD THAT RETURNS EVERY IMAGE,
+        # WHITOUT A PARAMETER THE SERVICE RETURNS ONLY THE FIRST PAGE
+        $IMAGES = $self->_execute(
+            endpoint    => 'http://interview.agileengine.com',
+            method      => 'GET',
+            path        => '/images?limit=20'
+        );
+    }
+
+    return $IMAGES;
+}
+
+sub get_images {
+    my ($self, %args) = @_;
+    my $page = $args{page} || 1;
+
+    my $json_response = {
+        has_more    => 1,
+        page        => $page,
+        pictures    => []
+    };
+
+    my $init_value = ($page - 1) * ITEMS_PER_PAGE;
+    my $final_value = $init_value + ITEMS_PER_PAGE;
+    my @pictures;
+    for (my $i=$init_value; $i < $final_value; $i++){
+        push @pictures, @{$IMAGES->{pictures}}[$i];
+    }
+    $json_response->{pictures} = \@pictures;
+
+    return JSON::XS::encode_json($json_response);
+}
+
+sub get_single_image {
     my ($self, %args) = @_;
 
     print "Content-type:text/html\n\n";
     print "<h1>Entro al paquete</h1>";
 
     print "<p>...".$args{id}."...</p>";
+    
+    print "<p>....$IMAGES...</p>";
 
     return;
 }
@@ -25,6 +92,55 @@ sub search_image {
     print "<p>...".$args{search_value}."...</p>";
 
     return;
+}
+
+sub _execute {
+    my ($self, %args) = @_;
+
+    my $endpoint = $args{endpoint} || undef;
+
+    return undef unless $endpoint;
+
+    my $ua;
+    {
+        $ua = LWP::UserAgent->new(
+            agent       => undef,
+            ssl_opts    => {
+                SSL_version => 'SSLv23:!SSLv2:!SSLv3', # disable SSLv2 and SSLv3 due to vulnerabilities
+            },
+            timeout     => 10,
+        );
+    };
+
+    my @headers = [
+        accept              => 'application/json',
+        content_type        => 'application/json',
+        $ACCESS_TOKEN ? (Authorization => "Bearer $ACCESS_TOKEN") : ()
+    ];
+
+    my $body = $args{body} ? JSON::XS::encode_json($args{body}) : undef;
+
+    my $request = HTTP::Request->new(
+        $args{method},
+        $endpoint.$args{path},
+        @headers,
+        $body
+    );
+
+    my $result = $ua->request($request);
+
+    return unless $result;
+
+    my $content = $result->content();
+    my $decoded_result = eval { JSON::XS::decode_json($content) };
+
+    if (my $error = $@) {
+        print STDERR ' - Could not decode success response: '.$error;
+
+        return undef;
+    }
+
+    return $decoded_result;
 }
 
 1;
